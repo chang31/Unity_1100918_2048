d45010da8a3a3bcefcdb009bbe86a4f4797f3658
chang31 <90996571+chang31@users.noreply.github.com>
2021-10-17 13:03:08 +0800

    學習場景存放C#

---
+++ Unity_1100918_2048/Assets/Script/Car.cs
@@ -0,0 +1,21 @@
+// 註解:說明文字，不執行
+// 功能 21.10.17
+// 開發者 Chang 21.10.17 
+using UnityEngine;  //  引用 UnityEngine API
+
+//class類別(藍圖)  名稱跟檔名相同
+public class Car : MonoBehaviour
+{
+    // 認識欄位 Field 儲存資料
+    // 語法:
+    // 資料類型 欄位名稱 結束符號
+    // 常用基本四大類型
+    // 1.整數 int
+    // 2.浮點數 有小數點 float
+    // 3.字串 文字資訊 string
+    // 4.布林值 是與否 true or false
+    int cc =2000;
+    float weight =3.5f;
+    string brang = "凌志";
+    bool hasSkyWindow = true;
+}
